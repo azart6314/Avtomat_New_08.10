32cff89a4c5931bb552fbeadd48b8fba2207b7d6
azart6314 <azart6314@gmail.com>
2023-10-09 13:14:23 +0300

    initial 2

---
+++ src/test/java/Tests/SimpleTest.java
@@ -0,0 +1,20 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class SimpleTest extends BaseTest {
+
+
+    @Test
+    public void test1(){
+        System.out.println("It is my 1 test");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("It is my 2 test");
+    }
+
+
+}
+
