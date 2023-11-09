c5217e732ba3e73d387373613411ef998b8aead4
azart6314 <azart6314@gmail.com>
2023-11-09 17:52:59 +0300

    урок 6 закончен

--- src/test/java/tests/XPathSelectorsTest.java
+++ src/test/java/tests/XPathSelectorsTest.java
@@ -25,59 +25,95 @@ public class XPathSelectorsTest {
     public void XPathLocatorTest() {
         driver.get("D:\\Teach_me_skills\\Avtomat_New_08.10\\Avtomat_New_08.10\\src\\test\\resources\\index.html");
 
-        // Абсолютный XPath ( пример будет использоватеь не будем )
+        /// сайт где описаны XPath https://www.w3schools.com/xml/xpath_syntax.asp
+
+
+        //<p href=”value”> (p -тег; href- атрибут; value -значение атрибута)
+        // Значение атрибута лучше брать в ковычки
+        // // - два слэша это значит пропустить не нужные уровни ( все XPath начинаются с // так как первый тэг html
+        // //* - поиск всех тегов которые присудствуют
+        //
+
+
+        // Абсолютный XPath ( Такого не должно быть так как при изменений чего то путь провалиться  )
         Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/div/div/h3/span[2]")).isDisplayed());
 
         //Все элементы на странице
         Assert.assertEquals(128, driver.findElement(By.xpath("//*")).isDisplayed());
 
         // Аналог поиска по tagName
+        //devToll - //h1
         Assert.assertTrue(driver.findElement(By.xpath("//h1")).isDisplayed());
 
         // Аналог родительского div и на один уровень ниже h1
+        //devToll - //div/h1
         Assert.assertTrue(driver.findElement(By.xpath("//div/h1")).isDisplayed());
 
         // Аналог родительского div и на любом уровене ниже div
+        //devToll - //div//div   -каждый слэш обозначает более глубокую вложенность
         Assert.assertTrue(driver.findElement(By.xpath("//div//div")).isDisplayed());
 
-        // Поиск элемента с тэгом div у которого есть аттрибут id
+        // Поиск элемента с тэгом div у которого есть аттрибут id но не знаем значения
+        //devToll -  //div[@id]
         Assert.assertTrue(driver.findElement(By.xpath("//div[@id]")).isDisplayed());
 
-        // Поиск элемента у которого есть аттрибут id cо значением top-logo
+        // Поиск элемента у которого есть аттрибут id cо значением top-logo и без разницы какой тэг
+        //devToll - //*[@id = 'top-logo']
         Assert.assertTrue(driver.findElement(By.xpath("//*[@id = 'top-logo']")).isDisplayed());
 
         // Поиск элемента у которого есть аттрибут method cо значением и аттрибут target со значением //можно использовать и or и and
+        //devToll - //*[@lang and @title]; //*[@method='post' and @target='_blank']
         Assert.assertTrue(driver.findElement(By.xpath("//*[@method='post' and @target='_blank']")).isDisplayed());
 
         // Поиск элемента c аттрибутом lang или class со значением
+        //devToll - //*[@lang or @class="noSel newsletter"]
         Assert.assertTrue(driver.findElement(By.xpath("//*[@lang or @class=\"noSel newsletter\"]")).isDisplayed());
 
 
-        //функций вроде как
+        //функций  xpath
         // Поиск элемента у которого значение аттрибута начинается с
-        Assert.assertTrue(driver.findElement(By.xpath("//*[starts-with(@id, 'new')]")).isDisplayed());
+        // devToll - //*[starts-with(@id, 'my-')]
+        Assert.assertTrue(driver.findElement(By.xpath("//*[starts-with(@id, 'my-')]")).isDisplayed());
 
         // Поиск элемента у которого значение аттрибута содержит подстроку
         Assert.assertTrue(driver.findElement(By.xpath("//div[contains(@id, 'Template')]")).isDisplayed());
 
-        // Поиск элемента у которого текстовое значение содержит равно
+        // Поиск элемента который содержит текстовое значение
+        // devToll - //*[text() = 'We are all animals!']
         Assert.assertTrue(driver.findElement(By.xpath("//*[text() = 'We are all animals!']")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//*[. = '<table>']")).isDisplayed()); // точку использовать очень аккуратно, когда текст в <> откр и закр тэгом
 
         // Поиск элемента у которого текстовое значение содержит подстроку
-        Assert.assertTrue(driver.findElement(By.xpath("//*[contains(text(), 'All Projects')]")).isDisplayed());
+        // devToll - //*[contains(text(), 'We ar all')]
+        Assert.assertTrue(driver.findElement(By.xpath("//*[contains(text(), 'We ar all')]")).isDisplayed());
 
-        // Поиск элемента по индексу
+        // Поиск элемента по индексу - желательно не использовать ( в дроп даунах можно )
+        // devToll - span[@class='markup'][3]
         Assert.assertTrue(driver.findElement(By.xpath("//div[@class = 'summary-links text-secondary']/a[2]")).isDisplayed());
 
-        // Поиск родителя у элемента с тэгом h1
+    }
+
+
+        @Test
+        public void XPathAxesTest() {
+            driver.get("D:\\Teach_me_skills\\Avtomat_New_08.10\\Avtomat_New_08.10\\src\\test\\resources\\index.html");
+
+            //https://www.w3schools.com/xml/xpath_axes.asp
+            // 6урок 2.00
+
+            // - /.. подняться на уровень выше
+
+            // Поиск родителя у элемента с тэгом h1
+            //devToll-  //span[@id='Lastname']/parent::* (пример из урока)
         Assert.assertTrue(driver.findElement(By.xpath("//h1/..")).isDisplayed());
-        Assert.assertTrue(driver.findElement(By.xpath("//h1/parent::div")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//h1/parent::*")).isDisplayed());
 
         // Поиск всех предков с тэгом div у элемента с тэгом h1
+        //devToll- //span[@id='Lastname']/ancestor::*
         Assert.assertTrue(driver.findElement(By.xpath("//h1/ancestor::div")).isDisplayed());
 
         // Использование child - непосредственно дочерние элементы с тэго a от div
+        // devToll- //span[@id='Lastname']/span  (пример из урока)
         Assert.assertTrue(driver.findElement(By.xpath("//div/a")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//div/child::a")).isDisplayed());
 
